97746b817dca078f0a211a22d42b7511edfcd20f
Jatin <35653770+jatin1201@users.noreply.github.com>
2019-05-15 12:10:46 +0530

    Attach Security Groups

---
+++ AttachSG.py
@@ -0,0 +1,14 @@
+ import boto3
+
+ ec2 = boto3.resource('ec2')
+  instances = ec2.instances.filter()
+  for instance in instances:
+     print(instance.id, instance.instance_type)
+     # Get a list of ids of all securify groups attached to the instance
+     all_sg_ids = [sg['GroupId'] for sg in instance.security_groups]
+     # Check the SG to be removed is in the list  
+     if sg_id in all_sg_ids:    
+     # Remove the SG from the list                                      
+       all_sg_ids.remove(sg_id)         
+     # Attach the remaining SGs to the instance                              
+       instance.modify_attribute(Groups=all_sg_ids)                 
