e8a74004099b9174922f425d5f0dc4a1de38f4fb
Jatin <35653770+jatin1201@users.noreply.github.com>
2019-05-15 19:05:53 +0530

    SG Open To World

---
+++ PortOpenForWorld.py
@@ -0,0 +1,34 @@
+import sys
+import boto
+from boto import ec2
+from boto import sns
+connection=ec2.connect_to_region("region-name")
+connSNS = boto.sns.connect_to_region("region-name")
+sg=connection.get_all_security_groups()
+ 
+listOfInstances=""
+messages="Following Instances have port XX open"
+ 
+def getTag(instanceId):
+    reservations=connection.get_all_instances(filters={'instance_id':instanceId})
+    for r in reservations:
+        for i in r.instances:
+            return i.tags['Name']
+ 
+try:
+    for securityGroup in sg:
+        for rule in securityGroup.rules:
+            global instanceId;
+            if (rule.from_port=='XX' and rule.to_port == 'XX') and '0.0.0.0/0' in str(rule.grants):
+                for instanceid in securityGroup.instances():
+                    instanceId=str(instanceid)
+                    listOfInstances += "Instance Name : " + getTag(instanceId.split(':')[1]) + "\t State:" + instanceid.state + "\t SecurityGroup:" +securityGroup.name + "\n"
+                    connSNS.publish(topic='SNS-topic-arn-endpoint',message = messages + "\n" + listOfInstances, subject='ProjectName : Server List with Port X Open')
+ 
+except :
+    print('Some Error occurred : ')
+    print(sys.exc_info())
+    connSNS.publish(topic='SNS-topic-arn-endpoint',message = sys.exc_info(), subject='script ended with error')
+
+    
+   
